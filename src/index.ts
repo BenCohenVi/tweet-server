f5fbd33aedf87eb41aa0c42934ccb71677b4a945
Ben Cohen <ben.cohenvi@gmail.com>
2021-04-26 15:16:08 +0300

    Initial server exposure

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello World!")
+import express from "express";
+
+const port = 3000;
+
+const app = express();
+
+app.get("/", (_req: express.Request, res: express.Response) => {
+  res.send("Well Done!");
+});
+
+app.listen(port, () => console.log(`The server is listening on port ${port}`));
