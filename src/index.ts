82ac405176be3486ae2bc7d69fa4d7fcc0f4c116
Ben Cohen <ben.cohenvi@gmail.com>
2021-06-03 08:30:52 +0300

    Add docker compose and env variables

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,9 @@ import { createConnection } from "typeorm";
 import tweetRouter from "./routes/tweet.route";
 import retweetRouter from "./routes/retweet.route";
 
-const port = 3000;
+const connectionString = process.env.DATABASE_URL;
+
+const port = process.env.PORT || 3000;
 
 const app = express();
 
@@ -14,7 +16,7 @@ app.use(express.json());
 app.use("/tweets", tweetRouter);
 app.use("/retweets", retweetRouter);
 
-createConnection()
+createConnection({ type: "postgres", url: connectionString })
   .then(() => {
     app.listen(port, () =>
       console.log(`The server is listening on port ${port}`)
